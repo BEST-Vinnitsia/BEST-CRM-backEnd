68f68537093b92ba88c76a214982463c22a0d6a9
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-12-27 10:49:42 +0200

    fixed module

--- src/modules/member/member.module.ts
+++ src/modules/member/member.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 
+// App
+import { AppDbService } from 'src/app.db.service';
+
 // Member
 import { MemberService } from './member/member.service';
 import { MemberDbService } from './member/member.db.service';
@@ -52,6 +55,8 @@ import { MembershipDbService } from './membership/membership.db.service';
 
     MembershipService,
     MembershipDbService,
+
+    AppDbService,
   ],
 })
 export class MemberModule {}
